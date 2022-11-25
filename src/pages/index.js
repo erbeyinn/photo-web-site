b583a3edb9e01942fde3499274060f05564ca16a
erbeyinn <eyup48.gs@gmail.com>
2022-11-26 02:42:29 +0300

    foto eklendi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,37 @@
 import React from "react"
-const IndexPage = () => (
-    <div>
-        <h1>kimi aradin</h1>
-    </div>
-)
+import {graphql} from "gatsby"
+import Img from "gatsby-image"
+import './index.css'
 
+const IndexPage = ({data}) => {
+    console.log(data)
+    return <div>
+        <h1>kimi aradin</h1>
+        <div className="photos">
+            {data.allFile.edges.map(({node}) => {
+                return <Img key={node.id} fluid={node.childImageSharp.fluid}/>
+            })}
+        </div>
+
+    </div>
+}
+
+export const pageQuery = graphql`
+{
+  allFile(filter:{absolutePath:{regex:"//photos//"}}) {
+    edges {
+       node {
+        id
+        childImageSharp{
+          fluid{
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+}
+`
 
 
 export default IndexPage
